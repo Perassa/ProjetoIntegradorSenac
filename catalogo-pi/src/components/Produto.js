8eac3053b82807f9e92b546665558485a281f8d8
Perassa <129907619+Perassa@users.noreply.github.com>
2024-02-23 22:22:37 -0300

    implementei a consulta na api

--- catalogo-pi/src/components/Produto.js
+++ catalogo-pi/src/components/Produto.js
@@ -1,21 +1,35 @@
 import Image from "next/image";
 
-export default function Produto() {
+export default function Produto(props) {
     return (
         <div class="card" style={{width:"18rem"}}>
   <Image src={"/assets/bike-01"}width={286} height={200} /> 
   <div class="card-body">
-    <h5 class="card-title">BICICLETA CALOI ELITE</h5>
-    <p class="card-text">Essa mountain bike aro 29 caloi, conta com câmbios Shimano Deore M6100 12v, freios a disco hidráulicos e suspensão Rockshox RS Judy Silver R29 com trava no guidão.
-Se você busca ingressar nas competições e está em dúvida sobre qual a melhor mtb ou qual a mountain bike aro 29 shimano com melhor custo benefício, a caloi elite  é a escolha ideal. Com essa bicicleta você vai ter a melhor experiência nas trilhas e competições.</p>
+    <h5 class="card-title">{props.nome}</h5>
+    <p class="card-text">{props.descricao}
+</p>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item"> Novidade</li>
-    <li class="list-group-item"> Disponível</li>
-    
-  </ul>
+  <div className="d-flex justify-content-between aling-items-center">
+    {
+      props.Disponível == false
+      ? <span className="badge text-bg-secondary">Indisponível</span>
+      :null
+    }
+    {
+      props.novidade == true
+      ? <span className=""></span>
+      :null
+    }
+    <span className="badge text-bg-secondary">Indisponível</span>
+    <span className="badge text-bg-success">Novidade</span>
+  </div>
   <div class="card-body">
     <h3> R$ 7.999,99</h3>
+
+    <p>Parcelas de 12x no cartão sem juros </p>
+    <div>
+    <Image src={"/assets/cartoes.png"}width={100} height={60} /> 
+    </div>
   </div>
 </div>
     )
