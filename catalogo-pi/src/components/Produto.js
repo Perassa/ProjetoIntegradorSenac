a0ed34e7db537e14727875d005b56c6a494e0e75
Perassa <129907619+Perassa@users.noreply.github.com>
2024-02-26 22:17:16 -0300

    melhora do layout

--- catalogo-pi/src/components/Produto.js
+++ catalogo-pi/src/components/Produto.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export default function Produto(props) {
     return (
-        <div class="card" style={{width:"18rem"}}>
-  <Image src={"/assets/bike-01"}width={286} height={200} /> 
-  <div class="card-body">
-    <h5 class="card-title">{props.nome}</h5>
-    <p class="card-text">{props.descricao}
+      <div className="col-3">
+        
+      
+        <div className="card" style={{width:"18rem"}}>
+  <Image alt="Foto" src={"/assets/bike-01"}width={286} height={200} /> 
+  <div className="card-body">
+    <h5 className="card-title">{props.nome}</h5>
+    <p className="card-text">{props.descricao}
 </p>
   </div>
   <div className="d-flex justify-content-between aling-items-center">
@@ -23,14 +26,15 @@ export default function Produto(props) {
     <span className="badge text-bg-secondary">Indisponível</span>
     <span className="badge text-bg-success">Novidade</span>
   </div>
-  <div class="card-body">
+  <div className="card-body">
     <h3> R$ 7.999,99</h3>
 
     <p>Parcelas de 12x no cartão sem juros </p>
     <div>
-    <Image src={"/assets/cartoes.png"}width={100} height={60} /> 
+    <Image alt="Foto" src={"/assets/cartoes.png"}width={100} height={60} /> 
     </div>
   </div>
+</div>
 </div>
     )
 }
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -1,61 +1,65 @@
 import Produto from "@/components/Produto";
 import axios from "axios";
 import Image from "next/image";
-import { useState} from "react"
+import { useEffect, useState} from "react"
 
 
 export default function Home() {
 
   const [listaProduto, setListaProduto] = useState([])
 
-  axios.get('https://localhost:7282/api/Produto')
-  .then(resp => setListaProduto(resp.data))
+ 
+
+  useEffect(() =>{
+    axios.get('https://localhost:7282/api/Produto')
+    .then(resp => setListaProduto(resp.data))
+  },[])
 
   return (
     <>
     
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <Image src={"/assets/logo.png"} width={150} height={60}/>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <Image alt="Foto" src={"/assets/logo.png"} width={150} height={60}/>
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Busca" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Busca</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Busca" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Busca</button>
       </form>
     </div>
   </div>
 </nav>
     
-      <div>
+      <div className="row">
         {
-          listaProduto.map((dado,index) => <Produto nome={dado.nome} descricao={dado.descricao} 
+          listaProduto.map((dado,index) => <Produto key={index} nome={dado.nome} descricao={dado.descricao} 
                 disponivel={dado.disponivel} novidade={dado.novidade} preco={dado.preco}/>)
         }
       </div>
