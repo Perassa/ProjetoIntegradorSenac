9d057bc87efddf03b89f95efcf714460117c8178
Perassa <129907619+Perassa@users.noreply.github.com>
2024-03-01 22:18:39 -0300

    terminamos a parte de cadastro

--- catalogo-pi/src/pages/cadastro.js
+++ catalogo-pi/src/pages/cadastro.js
@@ -3,16 +3,42 @@ import CustomInputLabel from "@/components/CustomInputLabel";
 import CustomTextAreaLabel from "@/components/CustomTextAreaLabel";
 import Cabecalho from "@/components/Cabecalho";
 import axios from "axios";
+import { useState } from "react";
 
 export default function cadastro(){
 
-    const [dadosProduto, setDadosProduto] = useState({})
 
+    const [dadosProduto, setDadosProduto] = useState({
+                                                      id : 0,
+                                                      nome : "",
+                                                      descricao : "",
+                                                      preco: 0,
+                                                      novidade: true,
+                                                      disponivel: true
+    })
+    
 function handleSalvar()
 {
     //Enviaremos o post para a API
     axios.post("https://localhost:7282/api/Produto", dadosproduto)
     .then(res => console.log(res))
+}
+ 
+function handleChange(e){
+   // console.log(e.target.value)
+   // console.log(e.target.id)
+
+   if (e.target.type == "checkbox")
+   {
+      dadosProduto[e.target.id] = e.target.checked
+   } else{
+      dadosProduto[e.target.id] = e.target.value
+   }
+
+    setDadosProduto(dadosProduto)
+
+    console.log (dadosProduto) 
+    
 }
 
     return (
@@ -20,12 +46,12 @@ function handleSalvar()
         <Cabecalho/>
         <div className="container-fluid">
             <div className="row">
-               <CustomInputLabel id="nome" texto="nome" col="8" />
-               <CustomInputLabel id="preco" texto="preço" col="4" />
-               <CustomTextAreaLabel id="descricao" texto="Descrição" col="12"/>
-               <CustomInputLabel id="imagem" texto="URL da Imagem" col="12"/>  
-               <CustomCheckBoxLabel id="novidade" texto="Novidade" col="6"/>  
-               <CustomCheckBoxLabel id="disponivel" texto="Disponivel" col="6"/>  
+               <CustomInputLabel id="nome" onChange={handleChange} texto="nome" col="8" />
+               <CustomInputLabel id="preco"onChange={handleChange} texto="preço" col="4" />
+               <CustomTextAreaLabel id="descricao"onChange={handleChange} texto="Descrição" col="12"/>
+               <CustomInputLabel id="imagem"onChange={handleChange} texto="URL da Imagem" col="12"/>  
+               <CustomCheckBoxLabel id="novidade"onChange={handleChange} texto="Novidade" col="6"/>  
+               <CustomCheckBoxLabel id="disponivel"onChange={handleChange} texto="Disponivel" col="6"/>  
             </div>
             <div className="row">
                 <div className="col-12 text-center">
