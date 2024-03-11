d0c43557a7383895f4a31b580949323ab2c10a4f
Perassa <129907619+Perassa@users.noreply.github.com>
2024-03-11 20:24:16 -0300

    finalidado o Projeto 11.03.2024

--- catalogo-pi/src/pages/cadastro.js
+++ catalogo-pi/src/pages/cadastro.js
@@ -7,68 +7,99 @@ import { useState } from "react";
 import Rodape from "@/components/Rodape";
 import { useRouter } from "next/router";
 
-export default function cadastro(){
-    const router = useRouter()
+export default function cadastro() {
+  const router = useRouter();
 
-    const [dadosProduto, setDadosProduto] = useState({
-                                                      id : 0,
-                                                      nome : "",
-                                                      descricao : "",
-                                                      preco: 0,
-                                                      novidade: true,
-                                                      disponivel: true
-    })
-    
-    function handleSalvar()
-    {
-        //Enviaremos o post para a API
-        axios.post("https://localhost:7282/api/Produto", dadosProduto)
-            .then(res => {
-                console.log(res)
-                router.push("/")}
-                )
+  const [dadosProduto, setDadosProduto] = useState({
+    id: 0,
+    nome: "",
+    descricao: "",
+    preco: 0,
+    novidade: false,
+    disponivel: false,
+  });
+
+  function handleSalvar() {
+    dadosProduto.preco = dadosProduto.preco.replace(".", "");
+    dadosProduto.preco = dadosProduto.preco.replace(",", ".");
+
+    alert("Seu Produto foi salvo com sucesso");
+    //Enviaremos o post para a API
+    axios
+      .post("https://localhost:7282/api/Produto", dadosProduto)
+      .then((res) => {
+        console.log(res);
+        router.push("/");
+      });
+
     }
-   
- 
-function handleChange(e){
-   // console.log(e.target.value)
-   // console.log(e.target.id)
 
-   if (e.target.type == "checkbox")
-   {
-      dadosProduto[e.target.id] = e.target.checked
-   } else{
-      dadosProduto[e.target.id] = e.target.value
-   }
+  function handleChange(e) {
+    // console.log(e.target.value)
+    // console.log(e.target.id)
 
-    setDadosProduto(dadosProduto)
+    if (e.target.type == "checkbox") {
+      dadosProduto[e.target.id] = e.target.checked;
+    } else {
+      dadosProduto[e.target.id] = e.target.value;
+    }
 
-    console.log (dadosProduto) 
-    
-}
+    setDadosProduto(dadosProduto);
 
-    return (
-        <>
-        <Cabecalho/>
-        <div className="container-fluid">
-            <div className="row">
-               <CustomInputLabel id="name" onChange={handleChange} texto="nome" col="8" />
-               <CustomInputLabel id="preco"onChange={handleChange} texto="preço" col="4" />
-               <CustomTextAreaLabel id="descricao"onChange={handleChange} texto="Descrição" col="12"/>
-               <CustomInputLabel id="imagem"onChange={handleChange} texto="URL da Imagem" col="12"/>  
-               <CustomCheckBoxLabel id="novidade"onChange={handleChange} texto="Novidade" col="6"/>  
-               <CustomCheckBoxLabel id="disponivel"onChange={handleChange} texto="Disponivel" col="6"/>  
-            </div>
-            <div className="row">
-                <div className="col-12 text-center">
-                    <button className="btn btn-primary" onClick={handleSalvar}>Salvar</button>
-                </div>
-            </div>
+    console.log(dadosProduto);
+  }
+
+  return (
+    <>
+      <Cabecalho />
+      <div className="container-fluid">
+        <div className="row">
+          <CustomInputLabel
+            id="name"
+            onChange={handleChange}
+            texto="nome"
+            col="8"
+          />
+          <CustomInputLabel
+            id="preco"
+            onChange={handleChange}
+            texto="preço"
+            col="4"
+          />
+          <CustomTextAreaLabel
+            id="descricao"
+            onChange={handleChange}
+            texto="Descrição"
+            col="12"
+          />
+          <CustomInputLabel
+            id="imagem"
+            onChange={handleChange}
+            texto="URL da Imagem"
+            col="12"
+          />
+          <CustomCheckBoxLabel
+            id="novidade"
+            onChange={handleChange}
+            texto="Novidade"
+            col="6"
+          />
+          <CustomCheckBoxLabel
+            id="disponivel"
+            onChange={handleChange}
+            texto="Disponivel"
+            col="6"
+          />
         </div>
-        <Rodape></Rodape>
-        
-        </>
-        
-
-    )
+        <div className="row">
+          <div className="col-12 text-center">
+            <button className="btn btn-primary" onClick={handleSalvar}>
+              Salvar
+            </button>
+          </div>
+        </div>
+      </div>
+      <Rodape></Rodape>
+    </>
+  );
 }
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   function handlePesquisar(filtro){
     const valorFiltro = filtro.target.value
  console.log(listaProduto)
-    const filtrado = listaProduto.filter((dados) => dados.name.toLowerCase().includes(valorFiltro.toLowerCase()))
+    const filtrado = listaProduto.filter((dados) => dados.descricao.toLowerCase().includes(valorFiltro.toLowerCase()))
     
     setListaProdutoFiltrado(filtrado)
     console.log(filtro)
